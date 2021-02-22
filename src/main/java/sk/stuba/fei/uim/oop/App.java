e3e64a55718b050b3acc8a9c940d0ecba7e13ee2
Daniel-Fundarek <xfundarekd@stuba.sk>
2021-02-22 21:20:56 +0100

    compare strings

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -1,9 +1,13 @@
 package sk.stuba.fei.uim.oop;
 
+import java.util.Objects;
+
 public class App {
     public static void main(String[] args) {
 
-        int x = KeyboardInput.readInt();
-        System.out.println("Hello "+ x + " Nice");
+     String a = "a";
+     String c = KeyboardInput.readString("zadaj text");
+     System.out.println("Compare strings "+ a.equals(c));
+     System.out.println("Compare strings better way "+ Objects.equals(a,c));
     }
 }
