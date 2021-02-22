efc9d46a7400f7f11875468d97f4d22ebfd48c3b
Daniel-Fundarek <xfundarekd@stuba.sk>
2021-02-22 22:45:49 +0100

    pole cez ...

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -5,17 +5,10 @@ import java.util.Objects;
 //Pointre funguju vo funkcii cize ked zmenim nieco v poli tak sa to prenesie do mainu
 public class App {
     public static void main(String[] args) {
-        int[][] x = new int[10][9];
-        for(int i =0; i< x.length;i++) {
-            for(int j =0; j < x[i].length;j++)
-                x[i][j] = j+1;
-        }
+        vypis(1,5,0,39,57,5);
+    }
 
-        for(int i =0; i< x.length;i++) {
-            for(int j =0; j < x[i].length;j++)
-                System.out.print(x[i][j]+" ");
-            System.out.println();
-        }
-
+    public static void vypis(int... pole){
+        System.out.println(Arrays.toString(pole));
     }
 }
