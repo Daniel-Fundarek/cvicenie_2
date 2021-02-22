f39866e492469e4d5d849461072fd9aa8078790c
Daniel-Fundarek <xfundarekd@stuba.sk>
2021-02-22 22:03:38 +0100

    Pointre

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,13 +2,28 @@ package sk.stuba.fei.uim.oop;
 
 import java.util.Arrays;
 import java.util.Objects;
-
+//Pointre funguju vo funkcii cize ked zmenim nieco v poli tak sa to prenesie do mainu
 public class App {
     public static void main(String[] args) {
-        int[] x = new int[] {1,2,3};
-        int[] y = new int [3];
-        for (int i =0; i<y.length; i++)
-            y[i]=i+1;
-        System.out.println(Arrays.equals(x,y));
+        int x = 5;
+        int[] y = new int[]{1,2,3};
+        System.out.println(x);
+        pokusRaw(x);
+        System.out.println(x);
+
+        System.out.println(Arrays.toString(y));
+        pokusArray(y);
+        System.out.println(Arrays.toString(y));
     }
+
+    public static void  pokusRaw(int cislo){
+        cislo = 7;
+        System.out.println(cislo);
+    }
+    public static void  pokusArray(int[] array){
+        array[0] = 10;
+        System.out.println(Arrays.toString(array));
+
+    }
+
 }
